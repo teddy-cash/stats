d709b31757b028fced9b59dce529ff6125464303
Sugar Teddy <cashteddy@protonmail.com>
2021-10-26 18:03:14 +0200

    initial

---
+++ README.md
@@ -0,0 +1,21 @@
+# Slurper
+
+**TODO: Add description**
+
+## Installation
+
+If [available in Hex](https://hex.pm/docs/publish), the package can be installed
+by adding `slurper` to your list of dependencies in `mix.exs`:
+
+```elixir
+def deps do
+  [
+    {:slurper, "~> 0.1.0"}
+  ]
+end
+```
+
+Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
+and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
+be found at [https://hexdocs.pm/slurper](https://hexdocs.pm/slurper).
+
---
+++ lib/reduce.ex
@@ -0,0 +1,66 @@
+defmodule Reducer do
+  require Logger
+
+  @signature "LUSDBorrowingFeePaid(indexed address _borrower, uint256 _LUSDFee)"
+
+  def write do
+    {last_7d, total} = run()
+    str = Jason.encode!(%{
+      teddy_staking: %{
+        last_7d_yield: last_7d,
+        since_inception: total
+      }
+    })
+    File.write!("_build/data.json", str)
+  end
+
+  def run do
+    fees = []
+
+    lines = File.read!("activepool.json")
+    |> String.split("\n")
+
+    fees = Enum.reduce(lines, fees ,fn(line, acc) ->
+      case Jason.decode(line) do
+        {:ok, data} ->
+          ts = data["block_signed_at"]
+          if data["decoded"]["signature"] == @signature do
+            [_from, %{"value" => val}] = data["decoded"]["params"]
+            val = val |> String.to_integer()
+            val = val / :math.pow(10, 18)
+            if val > 0 do
+              acc ++ [%{
+                ts: data["block_signed_at"],
+                fee: val
+              }]
+            else
+              acc
+            end
+          else
+            acc
+          end
+
+        _ ->
+          acc
+      end
+    end)
+
+    from = DateTime.utc_now() |> DateTime.add(-7*24*60*60, :second)
+    last_7d = Enum.filter(fees, fn(%{ts: ts}) ->
+      {:ok, dt, _} = DateTime.from_iso8601(ts)
+      DateTime.compare(from, dt) == :lt
+    end)
+    |> Enum.reduce( 0, fn(%{fee: f}, acc) -> acc + f end)
+
+    total = Enum.reduce(fees, 0, fn(%{fee: f}, acc) -> acc + f end)
+
+    Enum.group_by(fees, fn(f) -> String.split(f.ts, "T") |> hd end, fn(f) -> f.fee end)
+    |> Enum.sort_by(fn({ts, fees}) -> ts end)
+    |> Enum.each(fn({ts, fees}) -> IO.puts("#{ts},#{Enum.sum(fees) |> trunc}") end)
+
+    IO.puts("Last 7d: #{last_7d |> trunc}")
+    IO.puts("Total: #{total |> trunc}")
+
+    {last_yd, total}
+  end
+end
